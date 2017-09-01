7988d936f849de3f3f3c95996844c5612e6e6296
Fang-Pen Lin <bornstub@gmail.com>
2017-09-01 15:33:56 -0700

    Export some properties for signature pad

--- ios/RCTSignaturePadManager.m
+++ ios/RCTSignaturePadManager.m
@@ -1,9 +1,17 @@
+#import <UIKit/UIKit.h>
+
 #import "RCTSignaturePadManager.h"
 #import "SignaturePad.h"
 
 @implementation RCTSignaturePadManager
 
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_VIEW_PROPERTY(debug, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(velocityFilterWeight, CGFloat)
+RCT_EXPORT_VIEW_PROPERTY(minWidth, CGFloat)
+RCT_EXPORT_VIEW_PROPERTY(maxWidth, CGFloat)
+RCT_EXPORT_VIEW_PROPERTY(minDistance, CGFloat)
 
 - (UIView *)view {
   return [[SignaturePad alloc] init];

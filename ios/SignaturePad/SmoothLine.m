8de53f1143522478920df1c27a75cef9beecdc88
Fang-Pen Lin <bornstub@gmail.com>
2017-09-05 16:22:59 -0700

    Implement drawing

--- ios/SignaturePad/SmoothLine.m
+++ ios/SignaturePad/SmoothLine.m
@@ -7,6 +7,7 @@
 //
 
 #import "SmoothLine.h"
+#import "Bezier.h"
 #import "Utils.h"
 
 struct WidthTuple {
@@ -14,6 +15,12 @@ struct WidthTuple {
     CGFloat endWidth;
 };
 typedef struct WidthTuple WidthTuple;
+
+struct ControlPointTuple {
+    CGPoint control1;
+    CGPoint control2;
+};
+typedef struct ControlPointTuple ControlPointTuple;
 
 @implementation SmoothLine {
     NSMutableArray<NSValue *> *linePoints;
@@ -52,19 +59,48 @@ typedef struct WidthTuple WidthTuple;
     CGContextSetFillColorWithColor(context, self.color.CGColor);
 
     LinePoint points[4];
+    NSMutableArray<NSValue *> *cgPoints = [NSMutableArray arrayWithCapacity:4];
     NSUInteger i = 0;
     for (NSValue *value in linePoints) {
         [value getValue:&points[i]];
+        [cgPoints addObject:[NSValue valueWithCGPoint:points[i].position]];
         ++i;
     }
 
-    WidthTuple widths = [self calculateCurveWidthsForStartPoint:points[1] endPoint:points[2]];
+    LinePoint startPoint = points[1];
+    LinePoint endPoint = points[2];
 
-    [self drawStraightLineWithContext:context
-                           startPoint:points[1].position
-                             endPoint:points[2].position
-                           startWidth:widths.startWidth
-                             endWidth:widths.endWidth];
+    WidthTuple widths = [self calculateCurveWidthsForStartPoint:startPoint endPoint:endPoint];
+
+    // check distance among all points, if we see too close, draw it with simple line instead
+    // as calculateCurveControlPoints cannot fit them
+    BOOL tooClose = [self pointsTooClose: cgPoints];
+
+    // points are not too close, draw as curve
+    if (!tooClose) {
+        CGPoint c2 = [self calculateCurveControlPoints:points[0].position
+                                                    s2:points[1].position
+                                                    s3:points[2].position].control2;
+        CGPoint c3 = [self calculateCurveControlPoints:points[1].position
+                                                    s2:points[2].position
+                                                    s3:points[3].position].control1;
+        Bezier *curve = [[Bezier alloc] initWithStartPoint:startPoint.position
+                                                  endPoint:endPoint.position
+                                                  control1:c2
+                                                  control2:c3];
+        [self drawCurveLineWithContext:context
+                                 curve:curve
+                            startWidth:widths.startWidth
+                              endWidth:widths.endWidth];
+    // looks like the point is too close, let's draw a striaght line
+    } else {
+        [self drawStraightLineWithContext:context
+                               startPoint:startPoint.position
+                                 endPoint:endPoint.position
+                               startWidth:widths.startWidth
+                                 endWidth:widths.endWidth];
+    }
+
 
     CGContextRestoreGState(context);
 
@@ -80,7 +116,7 @@ typedef struct WidthTuple WidthTuple;
                            endWidth:(CGFloat)endWidth
 {
     CGVector vector = CGVectorBetween(startPoint, endPoint);
-    NSUInteger steps = CGVectorLength(vector);
+    NSUInteger steps = fmax(CGVectorLength(vector), 1);
     [self drawLineWithContext:context
                         steps:steps
                    startWidth:startWidth
@@ -91,6 +127,21 @@ typedef struct WidthTuple WidthTuple;
            startPoint.y + vector.dy * time
        );
     }];
+}
+
+- (void)drawCurveLineWithContext:(CGContextRef)context
+                           curve:(Bezier *)curve
+                      startWidth:(CGFloat)startWidth
+                        endWidth:(CGFloat)endWidth
+{
+    NSUInteger steps = fmax([curve approximatedLengthWithSteps:10], 1);
+    [self drawLineWithContext:context
+                        steps:steps
+                   startWidth:startWidth
+                     endWidth:endWidth
+                     lineFunc:^CGPoint(CGFloat time) {
+                         return [curve pointAtTime:time];
+                     }];
 }
 
 - (void)drawLineWithContext:(CGContextRef)context
@@ -111,36 +162,6 @@ typedef struct WidthTuple WidthTuple;
         CGContextFillPath(context);
     }
 }
-
-/*
- // Draw a curve with startWidth as the initial and change over time to endWidth as the
- // final width
- private func drawCruve(
- context: CGContext,
- steps: CGFloat,
- startWidth: CGFloat,
- endWidth: CGFloat,
- lineFunc: ((_ atTime: CGFloat) -> CGPoint)
- ) {
- let widthDelta = endWidth - startWidth
- let drawSteps = UInt(floor(steps)) * 2
- for i in 0 ..< drawSteps {
- let t = CGFloat(i) / CGFloat(drawSteps)
- let ttt = t * t * t
- let point = lineFunc(t)
- // TODO: hmmm, not sure why t ^ 3 instead of just t?
- let width = startWidth + (ttt * widthDelta)
- context.addArc(
- center: point,
- radius: width,
- startAngle: 0,
- endAngle: 2 * CGFloat.pi,
- clockwise: false
- )
- context.fillPath()
- }
- }
-*/
 
 - (WidthTuple)calculateCurveWidthsForStartPoint:(LinePoint)startPoint endPoint:(LinePoint)endPoint {
     CGFloat newVelocity = [SmoothLine velocityFrom:startPoint to:endPoint] / 1000;
@@ -165,6 +186,64 @@ typedef struct WidthTuple WidthTuple;
 - (CGFloat)strokeWidthForVelocity:(CGFloat)velocity force:(CGFloat)force {
     return fmax((self.maxWidth / (velocity + 1)) * force, self.minWidth);
 }
+
+/// Determine if given points in a line, along the line, any pair of them is too close
+// (less than minDistance)
+- (BOOL)pointsTooClose:(NSArray<NSValue *> *)points {
+    CGPoint previousPoint;
+    [points[0] getValue:&previousPoint];
+    for (NSValue *value in [points subarrayWithRange:NSMakeRange(1, points.count - 1)]) {
+        CGPoint currentPoint;
+        [value getValue:&currentPoint];
+        CGVector vector = CGVectorBetween(previousPoint, currentPoint);
+        CGFloat distance = CGVectorLength(vector);
+        if (distance < self.minDistance) {
+            return YES;
+        }
+        previousPoint = currentPoint;
+    }
+    return NO;
+}
+
+/// Calculate Bezier curve control points for given 3 points
+/// implementation references to
+// https://github.com/szimek/signature_pad/blob/master/src/signature_pad.js#L267-L292
+- (ControlPointTuple) calculateCurveControlPoints:(CGPoint)s1 s2:(CGPoint)s2 s3:(CGPoint)s3 {
+    CGFloat dx1 = s1.x - s2.x;
+    CGFloat dy1 = s1.y - s2.y;
+    CGFloat dx2 = s2.x - s3.x;
+    CGFloat dy2 = s2.y - s3.y;
+
+    CGPoint m1 = CGPointMake(
+        (s1.x + s2.x) / 2.0,
+        (s1.y + s2.y) / 2.0
+    );
+    CGPoint m2 = CGPointMake(
+        (s2.x + s3.x) / 2.0,
+        (s2.y + s3.y) / 2.0
+    );
+
+    CGFloat l1 = sqrt((dx1 * dx1) + (dy1 * dy1));
+    CGFloat l2 = sqrt((dx2 * dx2) + (dy2 * dy2));
+
+    CGFloat dxm = m1.x - m2.x;
+    CGFloat dym = m1.y - m2.y;
+
+    CGFloat k = l2 / (l1 + l2);
+    CGPoint cm = CGPointMake(
+        m2.x + (dxm * k),
+        m2.y + (dym * k)
+    );
+
+    CGFloat tx = s2.x - cm.x;
+    CGFloat ty = s2.y - cm.y;
+
+    ControlPointTuple tuple;
+    tuple.control1 = CGPointMake(m1.x + tx, m1.y + ty);
+    tuple.control2 = CGPointMake(m2.x + tx, m2.y + ty);
+    return tuple;
+}
+
 
 
 /// Calculate velocity from source to dest

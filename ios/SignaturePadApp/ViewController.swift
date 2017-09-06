816c7e6cc4bb9f7072c61b7c4683a449033e4fa1
Fang-Pen Lin <bornstub@gmail.com>
2017-09-05 17:45:35 -0700

    Use layoutSubviews instead for getting the new size

--- ios/SignaturePadApp/ViewController.swift
+++ ios/SignaturePadApp/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let pad = (view as! SignaturePad)
         // pad.debug = ProcessInfo.processInfo.environment["DEBUG"] == "1"
         // Do any additional setup after loading the view, typically from a nib.
     }

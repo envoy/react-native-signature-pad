451d807a4c7fae2b7acd1211ecb04f6f74f918de
Fang-Pen Lin <bornstub@gmail.com>
2017-09-06 11:38:28 -0700

    Ignore stuff we don't need in npm package

--- ios/SignaturePad/RCTSignaturePadManager.m
+++ ios/SignaturePad/RCTSignaturePadManager.m
@@ -7,11 +7,11 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_VIEW_PROPERTY(debug, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(velocityFilterWeight, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(minWidth, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(maxWidth, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(minDistance, CGFloat)
+RCT_EXPORT_VIEW_PROPERTY(color, UIColor)
 
 - (UIView *)view {
   return [[SignaturePad alloc] init];
